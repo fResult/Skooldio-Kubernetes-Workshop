3e401eaac9b806d73b0b21b5306a898be4b99a57
fResult Sila <Styxmaz@gmail.com>
2024-02-14 21:37:34 +0700

    Add initial data for rating and script for setting environment variables for mongodb

---
+++ k8s/ratings/databases/script.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+set -e mongoimport --host localhost --username $MONGODB_USERNAME --password $MONGODB_PASSWORD \
+  --db $MONGODB_DATABASE --collection ratings --drop --file /docker-entrypoint-initdb.d/ratings_data.json
